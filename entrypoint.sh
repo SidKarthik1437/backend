c20eac843293d984000f0b39e6a295a047b0ae0b
Sid Karthik <sa268592@gmail.com>
2024-03-23 13:37:17 +0530

    deployment changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,6 @@
 python manage.py makemigrations
 python manage.py migrate --no-input
 # python manage.py runserver 0.0.0.0:8000
+python manage.py create_groups
+python manage.py populate_departments
+gunicorn --workers=3 --bind 0.0.0.0:8000 nexa.wsgi:application
