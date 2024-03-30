576a77b8c1288b616be749e96efab4fe89aba253
Sid Karthik <sa268592@gmail.com>
2024-03-30 12:28:41 +0530

    postgres

--- nexa/settings.py
+++ nexa/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = "nexa.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nexa', 
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'db', 
+        'PORT': '5432',
     }
 }
 
